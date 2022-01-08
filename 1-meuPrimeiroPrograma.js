8793470cf221422b0419da6cf82a5ee0828bde5a
Alexandre Sousa Dias Junior <jrdiiaas@icloud.com>
2022-01-08 12:55:45 -0300

    Executando operações aritméticas

--- 1-meuPrimeiroPrograma.js
+++ 1-meuPrimeiroPrograma.js
@@ -1,1 +1,19 @@
 console.log("Olá, este é o meu primeiro program em JavaScript");
+console.log("Trabalhando com variáveis");
+
+console.log("Minha idade é: ");
+
+const idade = 26;
+console.log(idade);
+
+console.log("Se eu somar 2, minha idade fica: ");
+console.log(idade + 2);
+
+console.log("É possível escrever o mesmo resultado de outra forma, veja: ");
+const idadeSomada = idade + 2;
+console.log(idadeSomada);
+
+console.log("Vamos concatenar? Juntar variável com texto e imprimílos?")
+console.log("Primeiro iremos diminuir 2, da ideade original, o resultado é: 26 - 2 = ", + idade - 2);
+console.log("Agora vamos dividir por 2, o resultado de 26 / 2 é: ", + idade / 2);
+console.log("Por fim vamos multiplicar por 2, teremos: 26 * 2 = ", + idade * 2);
