0cab8923629d94f737933859019b25eb502f8b8f
Alexandre Sousa Dias Junior <jrdiiaas@icloud.com>
2022-01-09 17:23:49 -0300

    Adicionando novo arquivo 3-operacoesAritmeticas

---
+++ 3-operacoesAritmeticas.js
@@ -0,0 +1,5 @@
+console.log("Operações Aritméticas");
+console.log("No JS a ordem de exceução dos cálculos seguem os padrões da matemática, logo, veja o exemplo a seguir:");
+console.log("10 + 8 * 2 = ", 10 + 8 * 2);
+console.log("Ou ainda, usando os mesmos números, mas aplicando uma ordem de execução diferente, teremos o seguinte:");
+console.log("(10 + 8) * 2 = ", (10 + 8) * 2);
