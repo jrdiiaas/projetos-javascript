c6e9dc239ac4d4ff462955c4804c274af1be727d
Alexandre Sousa Dias Junior <jrdiiaas@icloud.com>
2022-01-09 17:10:00 -0300

    Registrando considerações importantes sobre declarações de variáveis

--- 2-trabalhandoComVariaveis.js
+++ 2-trabalhandoComVariaveis.js
@@ -1,3 +1,5 @@
 //Console.log("Trabalhando com Variáveis");
 console.log("Trabalhando com Variáveis");
 //Observe, JS é case senstive, ou seja, trata-se de uma linguagem sensível à Masiúsculo e Minúsclo
+console.log("Importante: no JS você pode dar valor a uma variável sem precisar declarar o tipo de variável, todavia, se dessa forma for condicionada, essa variável terá Escopo Global, o que não é recomendado, inclusive é considerado uma má prática, para projetos e para segurança de execução dos projetos.");
+console.log("Então, toda vez que for usar uma variável, é importante declarar essa variável usando var, const ou let. Mais na frente entenderemos a distinção entre essas formas de declarar variáveis.")
