f39c87313e1573304a8f72ca3d2cceaaf3552f8a
Alexandre Sousa Dias Junior <jrdiiaas@icloud.com>
2022-01-09 16:55:11 -0300

    Programa 2 - Adicionando novo programa JS - 2-trabalhandoComVariaveis

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,3 @@
+//Console.log("Trabalhando com Variáveis");
+console.log("Trabalhando com Variáveis");
+//Observe, JS é case senstive, ou seja, trata-se de uma linguagem sensível à Masiúsculo e Minúsclo
