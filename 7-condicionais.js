3ecc25ea58a5380b075494c7c11ffee1ffdd816d
Alexandre Sousa Dias Junior <jrdiiaas@icloud.com>
2022-01-10 15:01:50 -0300

    Adicionando arquivo 7-condicionais

---
+++ 7-condicionais.js
@@ -0,0 +1,9 @@
+console.log(`Trabalhando com Condicionais`);
+
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+);
+listaDeDestinos.push(`Curitiba`) //Adicionando outro item na lista através do .push
+
