c809a0865eb80d608b4c11f0234118e5321a510d
Alexandre Sousa Dias Junior <jrdiiaas@icloud.com>
2022-01-10 14:53:40 -0300

    Finalizando o aprendizado com ARRAYs

--- 6-listas.js
+++ 6-listas.js
@@ -23,3 +23,13 @@ console.log(`Lista de destinos: ${listaDeDestinos}`);
 console.log(listaDeDestinos);
 
 //Também é possível adicionar novos ítens à lista, usando o .push, esse push vai empurrar itens pra dentro do ARRAY, veja a partir da linha 19.
+
+//Agora vamos aprender a remover do ARRAY com o comando splice
+console.log(`Iremos deletar São Paulo, é necessário informar a posição incial, no caso, SP está na segunda posição, e em seguida a quantidade de itens a serem deletados, dessa vez vamos deletar apenas 1, veja:`);
+//listaDeDestinos.splice(2, 1);
+console.log(`Veja a lista de destinos após a exclusão: ${listaDeDestinos}`);
+console.log(`Tivemos um erro na contagem, pois a contagem de posições inicia em zero, corrigindo para 1,1, veja:`); //comentaremos o primeiro splice.
+listaDeDestinos.splice(1, 1);
+console.log(`Veja a lista de destinos após a exclusão: ${listaDeDestinos}`);
+//Para ao invés de exibir todos os itens, caso queira realizar a consulta específica se um item da lista está disponível, pode-se usar a seguinte estrutura:
+console.log(listaDeDestinos[1], listaDeDestinos[0]);
