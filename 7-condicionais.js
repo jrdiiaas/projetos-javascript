75e0f5268bf2feb7f5eccca0eadb59389be875f6
Alexandre Sousa Dias Junior <jrdiiaas@icloud.com>
2022-01-10 16:28:40 -0300

    Adicionando primeiros aprendizados sobre condicionais

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -5,5 +5,24 @@ const listaDeDestinos = new Array(
     `São Paulo`,
     `Rio de Janeiro`
 );
-listaDeDestinos.push(`Curitiba`) //Adicionando outro item na lista através do .push
 
+console.log(`Lista de Destinos:`);
+console.log(listaDeDestinos);
+
+const idadeComprador = 15;
+
+if (idadeComprador >= 18) {
+    console.log(`Comprador maior de idade`);
+    listaDeDestinos.splice(1, 1);
+} else {
+    console.log(`Comprador NÃO é maior de idade, não posso vender`)
+}
+
+console.log(listaDeDestinos);
+
+//Outros operadores lógicos booleanos
+console.log(idadeComprador > 18);
+console.log(idadeComprador < 18);
+console.log(idadeComprador >= 18);
+console.log(idadeComprador <= 18);
+console.log(idadeComprador == 18);
