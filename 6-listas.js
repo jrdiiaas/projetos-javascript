f4c1078aeef8aeb71ca8c79bdd2e90fb892dc8c1
Alexandre Sousa Dias Junior <jrdiiaas@icloud.com>
2022-01-10 12:56:54 -0300

    Explicando o funcionamento do ARRAY

--- 6-listas.js
+++ 6-listas.js
@@ -5,3 +5,21 @@ const rioDeJaneiro = `Rio de Janeiro`;
 
 console.log(`Destinos possíveis:`);
 console.log(salvador, saoPaulo, rioDeJaneiro);
+
+//Listas são chamadas de ARRAYS
+//A maneira de declarar um ARRAY é diferente do modo como as variáveis são declaradas normalmente
+
+console.log(`Agora vamos imprimir a Lista de Destinos usando o ARRAY, veja:`);
+
+//Abaixo está o CONSTRUTOR de ARRAY
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+);
+listaDeDestinos.push(`Curitiba`) //Adicionando outro item na lista através do .push
+
+console.log(`Lista de destinos: ${listaDeDestinos}`);
+console.log(listaDeDestinos);
+
+//Também é possível adicionar novos ítens à lista, usando o .push, esse push vai empurrar itens pra dentro do ARRAY, veja a partir da linha 19.
