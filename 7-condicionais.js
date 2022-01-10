3d56c58ce9b418ff94c1383c9240ed38cc912e93
Alexandre Sousa Dias Junior <jrdiiaas@icloud.com>
2022-01-10 17:12:09 -0300

    Adicionando mais operadores lógicos dentro do IF

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -31,6 +31,7 @@ console.log(idadeComprador == 18);
 console.log(`Vamos adicionar mais IFs?`);
 
 const estaAcompanhado = true;
+const temPassagemComprada = true;
 
 console.log(`Comprador está acompanhado? ${estaAcompanhado}`);
 
@@ -43,6 +44,31 @@ if (idadeComprador >= 18) {
     listaDeDestinos.splice(1, 1);
 } else {
     console.log(`Comprador NÃO é maior de idade, NÃO posso vender`);
+}
+
+console.log(listaDeDestinos);
+
+//Melhorando o código
+
+console.log(`Código após melhoria: comprador está acompanhado? ${estaAcompanhado}`);
+
+if (
+    idadeComprador >= 18 || //Operador de ou
+    estaAcompanhado // Adicionando segunda condição dentro do IF
+) {
+    console.log(`Poderá realizar a compra`);
+    listaDeDestinos.splice(1, 1);
+    //A pessoa é menor de idade
+} else {
+    console.log(`Comprador NÃO é maior de idade, NÃO posso vender`);
+}
+
+console.log(`No embarque, está com a passagem? \n\n`); // O \n pula a linha
+
+if (idadeComprador >= 18 && temPassagemComprada == true) {
+    console.log(`Boa viagem!!!`);
+} else {
+    console.log(`Infelizmente não será possível embarcar!`);
 }
 
 console.log(listaDeDestinos);
