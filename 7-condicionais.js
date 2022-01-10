1b59e24593eab69d95fed39414f4d25a1b5de35c
Alexandre Sousa Dias Junior <jrdiiaas@icloud.com>
2022-01-10 16:51:52 -0300

    Adicionando informações sobre condicionais aninhadas

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -26,3 +26,23 @@ console.log(idadeComprador < 18);
 console.log(idadeComprador >= 18);
 console.log(idadeComprador <= 18);
 console.log(idadeComprador == 18);
+
+//Vamos adicionar a situação onde o menor de idade esteja acompanhado por uma pessoa maior de idade ou não veja:
+console.log(`Vamos adicionar mais IFs?`);
+
+const estaAcompanhado = true;
+
+console.log(`Comprador está acompanhado? ${estaAcompanhado}`);
+
+if (idadeComprador >= 18) {
+    console.log(`Comprador maior de idade`);
+    listaDeDestinos.splice(1, 1);
+    //A pessoa é menor de idade
+} else if (estaAcompanhado) {
+    console.log(`Comprador NÃO é maior de idade, mas ESTÁ ACOMPANHADO`);
+    listaDeDestinos.splice(1, 1);
+} else {
+    console.log(`Comprador NÃO é maior de idade, NÃO posso vender`);
+}
+
+console.log(listaDeDestinos);
