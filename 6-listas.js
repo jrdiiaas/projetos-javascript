92b30148ff578232199ccb7323f4983ac011be3f
Alexandre Sousa Dias Junior <jrdiiaas@icloud.com>
2022-01-10 12:42:05 -0300

    Entendendo o motivo da necessidade de otimizar listas

--- 6-listas.js
+++ 6-listas.js
@@ -1,1 +1,7 @@
-console.log("Trabalhando com Listas")
+console.log(`Trabalhando com Listas`);
+const salvador = `Salvador`;
+const saoPaulo = `São Paulo`;
+const rioDeJaneiro = `Rio de Janeiro`;
+
+console.log(`Destinos possíveis:`);
+console.log(salvador, saoPaulo, rioDeJaneiro);
